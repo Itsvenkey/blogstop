ad213445d48a0845578e3165a4a4dba234c63e0c
Itsvenkey <itsvenkey9@gmail.com>
2023-12-31 15:25:42 +0530

    allowed host set to all

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ni=e0cng^46k*psg0i92u9m6fkcz(c^p$u&^vk4epf7vxv)rkv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
